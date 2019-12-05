c967beb7c485f5ef1b79d6fac2ece02fca8febfc
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-12-05 18:33:25 +0000

    take images out

--- examples/ar1/coupling-time.R
+++ examples/ar1/coupling-time.R
@@ -20,7 +20,6 @@ ct = vector("numeric",nd)
 for (i in 1:nd) {
     arr = car1(n=n, N=N, a=a)
     dif = as.matrix(1*(arr[,,1]-arr[,,2] != 0.0))
-    image(dif)
     cc = rowSums(dif)
     ct[i] = (1:n)[cc < 0.5][1]
     message(paste(ct[i]," "), appendLF=FALSE)
@@ -35,7 +34,6 @@ rct = vector("numeric",nd)
 for (i in 1:nd) {
     arr = refcar1(n=n, N=N, a=a)
     dif = as.matrix(1*(arr[,,1]-arr[,,2] != 0.0))
-    image(dif)
     cc = rowSums(dif)
     rct[i] = (1:n)[cc < 0.5][1]
     message(paste(ct[i]," "), appendLF=FALSE)
